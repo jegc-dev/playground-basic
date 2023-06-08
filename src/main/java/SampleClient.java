9292c2d1fecdab5af4f0866a5247a52d74691e72
jgranados <jorge@thinairlabs.ca>
2023-06-07 20:36:57 -0600

    - Create a text file containing 20 different last names - Load data in a List

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -1,14 +1,25 @@
+import model.PatientCsv;
 import model.PatientDto;
 import service.PatientServiceImpl;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SampleClient {
 
 
     public static void main(String[] theArgs) {
 
-        getPatientByFamilySorted("smith");
+        try {
+            getCSVFileStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //getPatientByFamilySorted("smith");
 
     }
 
@@ -21,5 +32,24 @@ public class SampleClient {
         for (PatientDto p : results){
             System.out.println(p.toString());
         }
+    }
+
+    public static void getCSVFileStream() throws IOException {
+
+        Path path = FileSystems.getDefault().getPath("src","main","resources","patients.csv");
+
+        List<PatientCsv> patientsCsv = Files.lines(path)
+                .map(SampleClient::getPatient)
+                .collect(Collectors.toList());
+
+        System.out.println(patientsCsv);
+
+    }
+
+    public static PatientCsv getPatient(String line){
+        String[] fields = line.split(",");
+        if(fields.length != 2)
+            throw new RuntimeException("Invalid CSV line -" + line);
+        return new PatientCsv(fields[0],fields[1]);
     }
 }
---
+++ src/main/java/model/PatientCsv.java
@@ -0,0 +1,36 @@
+package model;
+
+public class PatientCsv {
+
+
+    String firstName;
+    String lastName;
+    public PatientCsv(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "PatientCsv{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
